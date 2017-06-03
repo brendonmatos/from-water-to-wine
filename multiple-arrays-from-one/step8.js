b3396fafa76b645b3b2b42cb007160ad1a0eada1
Brendon Ferreira <brendonferreiradm@gmail.com>
2017-06-03 19:58:12 -0300

    Create step8.js

---
+++ multiple-arrays-from-one/step8.js
@@ -0,0 +1,19 @@
+
+
+const numbers = [ 1,2,3,4,5 ]
+
+const powFilter = num => num * num
+const toStringFilter = num => num.toString()
+const toNegative = num => -num
+
+const transformWith = (fns, input) => (item, ky) => {
+	return Array.from(input, fns[ky])
+}
+
+// Por indicação do @suissa
+const fns = [ powFilter, toStringFilter, toNegative]
+const length = fns.length
+
+const r = Array.from({length}).map( transformWith( fns, numbers ) )
+
+console.log(r);
